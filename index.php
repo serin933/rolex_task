e1e285ac4743b595d0a2497ce4a385a287ed7566
serin933 <galstyan.ser1996@gmail.com>
2022-10-17 13:11:19 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+            <img width="75" src="./images/rolex_icon.webp">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link"  href="#">Date Just</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Date Just Pearl</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link " href="#">Day-Date</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                
+                <button class="btn btn-outline-success" type="submit" style="">Contact us</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="big_container">
+     <header class="banner">
+       <div class="image">
+         <img width="100%" height="300px" src="./images/rolex_banner.jpg">
+       </div>  
+     </header>
+       <main>
+        <div class="container">
+            <div class="row">
+                <div style="padding: 10px" class="mt-3 col-sm-6 col-md-4 col-lg-4">
+                    <a href="#" style="text-decoration:none; color: black"> 
+                        <img style="width:100%" src="./images/100001.png">
+                        <div class="mt-3" style="text-align:center">
+                            Datejust 36
+                        </div>
+                    </a>
+                </div>
+                <div style="padding: 10px" class="mt-3 col-sm-6 col-md-4 col-lg-4">
+                    <a href="#" style="text-decoration:none; color: black"> 
+                        <img style="width:100%" src="./images/100003.png">
+                        <div class="mt-3" style="text-align:center">
+                            Datejust 36
+                        </div>
+                    </a>
+                </div>
+                <div style="padding: 10px" class="mt-3 col-sm-6 col-md-4 col-lg-4">
+                    <a href="#" style="text-decoration:none; color: black"> 
+                        <img style="width:100%" src="./images/100004.png">
+                        <div class="mt-3" style="text-align:center">
+                            Datejust 36
+                        </div>
+                    </a>
+                </div>
+                <div style="padding: 10px" class="mt-3 col-sm-6 col-md-4 col-lg-4">
+                    <a href="#" style="text-decoration:none; color: black"> 
+                        <img style="width:100%" src="./images/100004.png">
+                        <div class="mt-3" style="text-align:center">
+                            Datejust 36
+                        </div>
+                    </a>
+                </div>
+                <div style="padding: 10px" class="mt-3 col-sm-6 col-md-4 col-lg-4">
+                    <a href="#" style="text-decoration:none; color: black"> 
+                        <img style="width:100%" src="./images/100005.png">
+                        <div class="mt-3" style="text-align:center">
+                            Datejust 36
+                        </div>
+                    </a>
+                </div>
+                <div style="padding: 10px" class="mt-3 col-sm-6 col-md-4 col-lg-4">
+                    <a href="#" style="text-decoration:none; color: black"> 
+                        <img style="width:100%" src="./images/100006.png">
+                        <div class="mt-3" style="text-align:center">
+                            Datejust 36
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        </main>
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
